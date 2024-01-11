4d78841905306bb18b30ea53efb4dba91ce657ff
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-01-11 09:48:42 +0100

    removed useEffect

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Vortex } from "react-loader-spinner";
 
