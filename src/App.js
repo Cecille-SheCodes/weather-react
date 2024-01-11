5a8ef4e6a3be6a46bd05916d9454802fc2e3ea2a
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-01-11 09:57:45 +0100

    added contact details

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
         <h1 className="header">My Weather App</h1>
         <br/>
         <Weather  />
-      
+        <br/>
+        <p>Designed by <a href="https://www.shecodes.io/graduates/82140-cecille-speckmaier">CMSpeckmaier</a></p>
+        <a href="https://github.com/Cecille-SheCodes/weather-react"> and is open-sourced in GitHub</a>
     </div>
   );
 }
