9c3f051f65bf915be25777f3b2a863796c055f1e
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2024-01-13 17:04:23 +0100

    added target for links

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import Weather from './Weather.js';
 function App() {
   return (
     <div className="App">
-     
-        <h1 className="header">My Weather App</h1>
-        <br/>
-        <Weather  />
-        <br/>
-        <p className="links">Designed by <a href="https://www.shecodes.io/graduates/82140-cecille-speckmaier">CMSpeckmaier</a> and open-sourced in <a href="https://github.com/Cecille-SheCodes/weather-react">GitHub</a></p>
-        
+      <h1 className="header">My Weather App</h1>
+      <br />
+      <Weather />
+      <br />
+      <p className="links">
+        Designed by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/82140-cecille-speckmaier"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CMSpeckmaier
+        </a>{" "}
+        and open-sourced in{" "}
+        <a
+          href="https://github.com/Cecille-SheCodes/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
